ab7dff5016d55c42e156166d7730a7131e9b1afd
Em Oaken <96680649+em-oaken@users.noreply.github.com>
2025-01-08 14:50:51 +0100

    Refactor body line generation

--- src/ant_one/app.py
+++ src/ant_one/app.py
@@ -9,6 +9,25 @@ from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
 
+def line_segments_gen(points, x_mirror=False):
+    """Generator for points in the same continuous segmented line
+    points: a list of coordinates for the line
+    x_mirror: defines if angles should be reversed to draw other side of the body
+    
+    Coordinates are in polar form: radius, theta in degrees for convenience
+    Points are relative to one another. Theta is in original reference system
+    """
+    x, y = 0, 0
+    for i in range(len(points)):
+        x += points[i][0] * math.cos(
+            points[i][1] * math.pi/180 * (-1)**(x_mirror)
+            )
+        y += -points[i][0] * math.sin(
+            points[i][1] * math.pi/180 * (-1)**(x_mirror)
+            )
+        yield round(x), round(y)
+
+
 class PlayScreen(toga.Box):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -65,6 +84,7 @@ class PimpScreen(toga.Box):
 
         ant_color = '#393A3E'
         
+        # Draw body
         with self.pimp_canvas.context.Fill(color=ant_color) as fill:
             ant_head = fill.ellipse(x=0, y=0, radiusx=20, radiusy=18)  # Head
         # with self.pimp_canvas.context.Fill(color="black") as fill:
@@ -72,45 +92,32 @@ class PimpScreen(toga.Box):
         # with self.pimp_canvas.context.Fill(color="black") as fill:
             ant_tail = fill.ellipse(x=110, y=0, radiusx=40, radiusy=20)  # Metasoma / Abdomen
         
-        def leg_generator(leg, x_mirror=False):
-            """Generate leg points
-            leg: defines if points of top, mid, or back leg
-            x_mirror: defines if angles should be reverses to draw other side of the body
-            
-            Coordinates are in polar form: radius, theta in degrees for convenience
-            Points are relative to one another. Theta is in original reference system
-            """
-            x, y = 0, 0
-            leg_points = {
-                'top': [(35, 0), (45, 90), (50, 150)],
-                'mid': [(45, 0), (50, 80), (50, 30)],
-                'back': [(60, 0), (40, 60), (50, 20)]
-            }
-            for i in range(len(leg_points[leg])):
-                x += leg_points[leg][i][0] * math.cos(
-                    leg_points[leg][i][1] * math.pi/180 * (-1)**(x_mirror)
-                    )
-                y += -leg_points[leg][i][0] * math.sin(
-                    leg_points[leg][i][1] * math.pi/180 * (-1)**(x_mirror)
-                    )
-                yield round(x), round(y)
+        # Draw legs
+        leg_points = {
+            'top': [(35, 0), (45, 90), (50, 150)],
+            'mid': [(45, 0), (50, 80), (50, 30)],
+            'back': [(60, 0), (40, 60), (50, 20)]
+        }
         
         with self.pimp_canvas.context.Stroke(0, 0, color=ant_color, line_width=5) as stroke, self.pimp_canvas.context.Fill(color=ant_color) as fill:
             for leg in ['top', 'mid', 'back']:
-                for i, coords in enumerate(leg_generator(leg)):
+                for i, coords in enumerate(line_segments_gen(leg_points[leg])):
                     if i == 0:
                         stroke.move_to(*coords)
                     else:
                         stroke.line_to(*coords)
                         fill.move_to(*coords)
                         fill.arc(*coords, radius=4)
-                for i, coords in enumerate(leg_generator(leg, x_mirror=True)):
+                for i, coords in enumerate(line_segments_gen(leg_points[leg], x_mirror=True)):
                     if i == 0:
                         stroke.move_to(*coords)
                     else:
                         stroke.line_to(*coords)
                         fill.move_to(*coords)
                         fill.arc(*coords, radius=4)
+
+        # Draw antennae
+        
 
 
 class AntOne(toga.App):
